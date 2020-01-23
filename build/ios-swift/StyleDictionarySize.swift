fb846f87d6d2bae2620ce8f7bba3688f41bc4c63
Mike Lock <michael.lockwitz@consensys.net>
2020-01-23 16:58:17 -0700

    Initial commit

---
+++ build/ios-swift/StyleDictionarySize.swift
@@ -0,0 +1,16 @@
+
+//
+// StyleDictionarySize.swift
+//
+// Do not edit directly
+// Generated on Thu, 23 Jan 2020 23:56:54 GMT
+//
+
+import UIKit
+
+public enum StyleDictionarySize {
+    public static let fontBase = CGFloat(16.00)
+    public static let fontLarge = CGFloat(32.00)
+    public static let fontMedium = CGFloat(16.00)
+    public static let fontSmall = CGFloat(12.00)
+}
